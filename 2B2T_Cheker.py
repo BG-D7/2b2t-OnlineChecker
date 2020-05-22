d1f18c9cdcdce025cc26cfa32c43fae57551ea76
BG-D7 <59792755+BG-D7@users.noreply.github.com>
2020-05-23 04:26:49 +0500

    Second Release

--- 2B2T_Cheker.py
+++ 2B2T_Cheker.py
@@ -1,19 +1,23 @@
 #IMPORTS
 from mcstatus import MinecraftServer
 import colorama
-import time
+import random
 #INITIALIZE
 colorama.init()
 server = MinecraftServer.lookup("2b2t.org")
+#INIT Functions
+def CheckerClean(a):
+	try:
+		status=server.status()
+		Response = colorama.Fore.BLACK + colorama.Back.GREEN + f"Онлайн: {status.players.online} игроков. Сервер ответил в {status.latency} ms"
+	except:
+		Response = colorama.Fore.BLACK + colorama.Back.RED + "Ошибка"
+	return Response
 #Checker
 print("2b2t online check by BG")
 while True:
 	# 'status' is supported by all Minecraft servers that are version 1.7 or higher.
 	#Trying to check server
-	try:
-		status = server.status()
-		print(colorama.Fore.BLACK + colorama.Back.GREEN + "The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
-	except:
-		print(colorama.Fore.BLACK + colorama.Back.RED + "Сервер не отвечает")
-	#Reset Style
+	CheckResponse = CheckerClean(1)
+	print(CheckResponse)
 	print(colorama.Style.RESET_ALL)
