aa9826b5e6a1143f8c2eb46592a26508945c354d
BG-D7 <59792755+BG-D7@users.noreply.github.com>
2020-07-07 16:35:31 +0500

    REBRAND

---
+++ MoneymodLite_color.py
@@ -0,0 +1,26 @@
+#IMPORTS
+from mcstatus import MinecraftServer
+import colorama
+import random
+#INITIALIZE
+colorama.init()
+server = MinecraftServer.lookup("2b2t.org")
+#INIT Functions
+def CheckerClean(a):
+	try:
+		status=server.status()
+		if status.players.online>=50:
+			Response = colorama.Fore.BLACK + colorama.Back.GREEN + f"Онлайн: {status.players.online} игроков. Рестарт вроде прошёл. Сервер ответил в {status.latency} ms"
+		else:
+			Response = colorama.Fore.BLACK + colorama.Back.YELLOW + f"Онлайн: {status.players.online} игроков. Рестарт вроде не прошёл. Сервер ответил в {status.latency} ms"
+	except:
+		Response = colorama.Fore.BLACK + colorama.Back.RED + "Ошибка"
+	return Response
+#Checker
+print(" Moneymod Lite by BG")
+while True:
+	# 'status' is supported by all Minecraft servers that are version 1.7 or higher.
+	#Trying to check server
+	CheckResponse = CheckerClean(1)
+	print(CheckResponse)
+	print(colorama.Style.RESET_ALL)
